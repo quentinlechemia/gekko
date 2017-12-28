441e1833a8950d01b8f7b241c9ed97b4c3723b35
Quentin Lechemia <ql@mybandmarket.com>
2017-12-28 22:58:10 +0100

    Update UI config

--- web/vue/UIconfig.js
+++ web/vue/UIconfig.js
@@ -4,19 +4,18 @@
 // see https://github.com/askmike/gekko/blob/stable/docs/installing_gekko_on_a_server.md
 
 const CONFIG = {
-  headless: false,
-  api: {
-    host: '127.0.0.1',
-    port: 3000,
-    timeout: 120000 // 2 minutes
-  },
-  ui: {
-    ssl: false,
-    host: 'localhost',
-    port: 3000,
-    path: '/'
-  },
-  adapter: 'sqlite'
+    headless: true,
+    api: {
+        host: '0.0.0.0',
+        port: 3000,
+    },
+    ui: {
+        ssl: false,
+        host: 'app-76f8c54e-e44c-4ad1-9a47-9bc5bc7b98fc.cleverapps.io', // Set this to the IP of the machine that will run Gekko
+        port: 3000,
+        path: '/'
+    },
+    adapter: 'sqlite'
 }
 
 if(typeof window === 'undefined')
