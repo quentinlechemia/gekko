062b485d654d2c5045e423e59129b5e6b88ed7f5
Quentin Lechemia <ql@mybandmarket.com>
2017-12-29 00:30:01 +0100

    New config

--- web/vue/UIconfig.js
+++ web/vue/UIconfig.js
@@ -11,7 +11,7 @@ const CONFIG = {
     },
     ui: {
         ssl: false,
-        host: '163.172.235.152', // Set this to the IP of the machine that will run Gekko
+        host: 'app-76f8c54e-e44c-4ad1-9a47-9bc5bc7b98fc.cleverapps.io', // Set this to the IP of the machine that will run Gekko
         port: 3000,
         path: '/'
     },
